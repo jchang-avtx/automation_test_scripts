d85221e04920313a7762a815cb5522aa9afe882a
ubuntu <edumanig@gmail.com>
2018-03-21 11:11:08 -0700

    check existing directory before install

--- Network_Testing_Scripts/aviatrix_terraform_installation.sh
+++ Network_Testing_Scripts/aviatrix_terraform_installation.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
+#setting up path for terraform
+cd $HOME
 
-# Setting up download path for terraform
+if [ ! -d $HOME/Aviatrix_Terraform ] ; then
+       mkdir $HOME/Aviatrix_Terraform
+fi
 cd $HOME/Aviatrix_Terraform
 
-mkdir $HOME/terraform
+if [ ! -d $HOME/terraform ] ; then
+       mkdir $HOME/terraform
+fi
 cd $HOME/terraform
-# Download terraform release version
+
+
 wget https://releases.hashicorp.com/terraform/0.11.4/terraform_0.11.4_linux_amd64.zip
 sudo apt-get install -y zip unzip
 unzip terraform_0.11.4_linux_amd64.zip
 
+
 sudo mv terraform /usr/local/bin/
 terraform -version
+#setting up path for GO
 
-# Setting up download path for GO
 cd $HOME/terraform
 
-# Download go lang version and extract
+
+
 wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
 
-# Setting up GOPATH
+#setting up GOPATH
 echo "export GOPATH=$HOME/work" > ~/.bash_profile
 source ~/.bash_profile
 export GOBIN=$HOME/work/bin
 echo "export GOPATH=$HOME/work" > ~/.zshrc
 source ~/.zshrc
 
-# Building the Aviatrix Plugin
+#Building the Aviatrix Plugin
 mkdir -p $GOPATH/src/github.com/terraform-providers
 cd $GOPATH/src/github.com/terraform-providers
 sudo apt install git 
 
-# git clone the latest Aviatrix provider APIs
 git clone https://github.com/AviatrixSystems/terraform-provider-aviatrix.git
 
 cd $GOPATH/src/github.com/terraform-providers/terraform-provider-aviatrix
@@ -46,7 +54,9 @@ echo 'providers {
   }' >> ~/.terraformrc
 
 # terraform configuration directory
-mkdir $HOME/test-Aviatrix
+if [ ! -d $HOME/test-Aviatrix ] ; then
+       mkdir $HOME/test-Aviatrix
+fi
 cd $HOME/test-Aviatrix
 terraform init
 echo
@@ -55,4 +65,3 @@ echo "Congratulations! You can now start building terraform configuration files 
 echo "--------------------------------------------------------------------------------------"
 echo
 bash
-echo
