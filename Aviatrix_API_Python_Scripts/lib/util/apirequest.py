b84aa451152c409f19c78e1e79393d67d76c2f62
Li Yan <lyan@aviatrix.com>
2018-03-27 15:34:52 -0700

    add retry for cloud api calls.

---
+++ Aviatrix_API_Python_Scripts/lib/util/apirequest.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2015-2018, Aviatrix Systems, Inc.
+'''
+Cloud API request mechanism.
+'''
+import logging
+from tools import retry
+
+
+class APIRequestError(Exception):
+    '''
+    API error.
+    '''
+    pass
+
+
+class APIRequest(object):
+    '''
+    Cloud API request class.
+    '''
+
+    def __init__(
+            self,
+            retries=8,
+            delay=1,
+            max_delay=None,
+            backoff=2,
+            retry_callback=None,
+            exceptions=None,
+            return_vals=None,
+            log=True):
+        self.log = log
+        self.logger = logging.getLogger('avx')
+        self.retries = retries
+        self.delay = delay
+        self.max_delay = max_delay
+        self.backoff = backoff
+        self.retry_callback = retry_callback
+        self.exceptions = exceptions
+        self.return_vals = return_vals
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, traceback):
+        pass
+
+    def execute(self, api, *args, **kwargs):
+        '''
+        Perform a cloud API call and return results.
+
+        '''
+        if self.log:
+            self.logger.info('Executing API request %s', api.__name__)
+        result = retry.invoke(
+            api,
+            fargs=args,
+            fkwargs=kwargs,
+            retry_callback=self.retry_callback,
+            exceptions=self.exceptions,
+            return_vals=self.return_vals,
+            tries=self.retries,
+            delay=self.delay,
+            max_delay=self.max_delay,
+            backoff=self.backoff)
+        return result
---
+++ Aviatrix_API_Python_Scripts/lib/util/retry.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2014-2018 Aviatrix, Inc.
+
+'''
+Retry mechanism for failure prone functions.
+'''
+
+import sys
+import logging
+import time
+from functools import partial
+
+
+class RetryError(Exception):
+    '''
+    Retry exception.
+    '''
+
+    def __init__(self, name, value, tries):
+        self.name = name
+        self.value = value
+        self.tries = tries
+
+    def __str__(self):
+        return '<Type: {} Value: {}>'.format(self.name, self.value)
+
+
+class RetValError(Exception):
+    '''
+    Function return value indicates failure.
+    '''
+    pass
+
+
+def _execute(func, return_vals):
+    '''
+    Execute a function and raise an exception if return value is found in the
+    tuple ret_vals.
+    '''
+    ret_val = func()
+    if return_vals and ret_val in return_vals:
+        raise RetValError('Function returned {}'.format(ret_val))
+    return ret_val
+
+
+def _retry_internal(
+        f,
+        retry_callback,
+        exceptions,
+        return_vals,
+        tries=-1,
+        delay=0,
+        max_delay=None,
+        backoff=1):
+    logger = logging.getLogger('avx')
+    _tries, _delay = tries, delay
+    if exceptions:
+        _exceptions = exceptions
+    else:
+        _exceptions = Exception
+
+    max_tries = _tries
+    while _tries:
+        try:
+            return _execute(f, return_vals)
+        except _exceptions as e:
+            exc_type, exc_value = sys.exc_info()[:2]
+            exc_obj = RetryError(exc_type.__name__, exc_value.message, max_tries - _tries + 1)
+            if retry_callback and not retry_callback(exc_obj):
+                raise
+            _tries -= 1
+            if not _tries:
+                raise
+            logger.warning(
+                '%s [%d/%d] failed: reason: %s, retrying in %s seconds...',
+                f.func.__name__,
+                _tries, max_tries,
+                e,
+                _delay)
+            time.sleep(_delay)
+            _delay *= backoff
+            if max_delay is not None:
+                _delay = min(_delay, max_delay)
+
+
+def invoke(
+        f,
+        fargs=None,
+        fkwargs=None,
+        retry_callback=None,
+        exceptions=None,
+        return_vals=None,
+        tries=-1,
+        delay=0,
+        max_delay=None,
+        backoff=1):
+    '''
+    Invokes a function and re-executes it if it failed.
+
+    : f: the function to execute.
+    : fargs: the positional arguments of the function to execute.
+    : fkwargs: the named arguments of the function to execute.
+    : retry_callback: A function which will be invoked after every attempt.
+      If this function returns false, retries will be aborted.
+    : exceptions: A tuple of exceptions to catch to retry.
+    : return_vals: A tuple of return values. Will retry if the function
+      returns any of these values.
+    : tries: the maximum number of attempts. default: -1 (infinite).
+    : delay: initial delay between attempts. default: 0.
+    : max_delay: the maximum value of delay. default: None (no limit).
+    : param backoff: multiplier applied to delay between attempts.
+      default: 1 (no backoff).
+    :returns: the result of the f function.
+    '''
+    args = fargs if fargs else list()
+    kwargs = fkwargs if fkwargs else dict()
+    return _retry_internal(
+        partial(f, *args, **kwargs),
+        retry_callback,
+        exceptions,
+        return_vals,
+        tries,
+        delay,
+        max_delay,
+        backoff)
