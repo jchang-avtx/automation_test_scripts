42fe540cb148a92883933dba1390c551422fef73
inc1t3Ful <ant.lee813@gmail.com>
2019-01-11 16:12:05 -0800

    Updated vpn_user_profile scripts
    * removed hardcoding

--- Terraform_Scripts/vpn_user_profile/vars.tf
+++ Terraform_Scripts/vpn_user_profile/vars.tf
@@ -1,3 +1,22 @@
 variable "aviatrix_controller_ip" {}
 variable "aviatrix_controller_username" {}
 variable "aviatrix_controller_password" {}
+
+variable "aviatrix_vpn_profile_name" {}
+variable "aviatrix_vpn_profile_base_rule" {}
+
+variable "aviatrix_vpn_profile_action" {
+  type = "list"
+}
+variable "aviatrix_vpn_profile_protocol" {
+  type = "list"
+}
+variable "aviatrix_vpn_profile_port" {
+  type = "list"
+}
+variable "aviatrix_vpn_profile_target" {
+  type = "list"
+}
+variable "aviatrix_vpn_profile_user_list" {
+  type = "list"
+}
