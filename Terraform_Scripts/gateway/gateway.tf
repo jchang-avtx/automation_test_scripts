d4607990d9b9a1ce9079895d7df179575df409a6
inc1t3Ful <ant.lee813@gmail.com>
2019-01-25 11:31:26 -0800

    Tested EIP parameters

--- Terraform_Scripts/gateway/gateway.tf
+++ Terraform_Scripts/gateway/gateway.tf
@@ -24,3 +24,22 @@ resource "aviatrix_gateway" "testGW1" {
 #        vpc_size = "${var.aws_instance[1]}"
 #         vpc_net = "${var.aws_vpc_public_cidr[1]}"
 # }
+
+##############################################
+## Case 2. Testing a gateway with EIP turned off
+## Be sure to comment out specific parts,if you want to test valid/invalid eip value;
+## Otherwise, be sure to fill out the EIP field as one specified in your pool in AWS
+##############################################
+# resource "aviatrix_gateway" "testGW1" {
+#         cloud_type = "${var.aviatrix_cloud_type_aws}"
+#       account_name = "${var.aviatrix_cloud_account_name}"
+#            gw_name = "${var.aviatrix_gateway_name[0]}"
+#             vpc_id = "${var.aws_vpc_id[0]}"
+#            vpc_reg = "${var.aws_region[0]}"
+#           vpc_size = "${var.aws_instance[0]}"
+#            vpc_net = "${var.aws_vpc_public_cidr[0]}"
+#   allocate_new_eip = "off"
+#                eip = "123.456.789.101" # input correct EIP here
+#              # eip = "54" # invalid IP format
+#              # eip = "" # empty value
+# }
