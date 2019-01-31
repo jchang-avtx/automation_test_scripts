68bca71107307c5379dcc58e3c393483707f982f
inc1t3Ful <ant.lee813@gmail.com>
2019-01-31 11:58:32 -0800

    Added saml_endpoint support for vpn_user
    ** tested saml_endpoint
    ++ added emptycreations test cases

--- Terraform_Scripts/vpn_user/vpn_user.tf
+++ Terraform_Scripts/vpn_user/vpn_user.tf
@@ -20,3 +20,13 @@ resource "aviatrix_vpn_user" "test_vpn_user3" {
   user_name   = "${var.aviatrix_vpn_user_name["user3"]}"
   user_email  = "${var.aviatrix_vpn_user_email["email3"]}"
 }
+
+## test SAML
+## comment out this section if you are not testing SAML along with in vars.tf and terraform.tfvars
+resource "aviatrix_vpn_user" "test_vpn_user4" {
+  vpc_id      = "${var.aws_vpc_id}"
+  gw_name     = "${var.aviatrix_gateway_name}"
+  user_name   = "${var.aviatrix_vpn_user_name["user4"]}"
+  user_email  = "${var.aviatrix_vpn_user_email["email4"]}"
+  saml_endpoint = "${var.aviatrix_vpn_user_saml[0]}"
+}
