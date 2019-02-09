5ac7b1f6219afa77c73763af0445dd7d2305db8f
inc1t3Ful <ant.lee813@gmail.com>
2019-02-08 17:12:33 -0800

    Added account_user testfiles
    ** standardized formatting for READMEs and testfiles

---
+++ Terraform_Scripts/account_user/vars.tf
@@ -0,0 +1,8 @@
+variable "aviatrix_controller_ip" {}
+variable "aviatrix_controller_username" {}
+variable "aviatrix_controller_password" {}
+
+variable "aviatrix_account_username" {}
+variable "aviatrix_cloud_account_name" {}
+variable "aviatrix_account_user_email" {}
+variable "aviatrix_account_user_password" {}
