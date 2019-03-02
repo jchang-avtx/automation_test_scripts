cb253262cb5ef2b16d8dade88ab84ced4cf372b5
Anthony <ant.lee813@gmail.com>
2019-03-01 17:43:21 -0800

    Added S2C dependency to testfile
    ++ added gateway creation to S2C to simulate on prem environment

--- Terraform_Scripts/site2cloud/site2cloud.tf
+++ Terraform_Scripts/site2cloud/site2cloud.tf
@@ -1,5 +1,29 @@
 ## Creates and manages an Aviatrix Site2Cloud connection
 
+# Create Aviatrix AWS gateway to act as our "Local"
+resource "aviatrix_gateway" "test_gateway1" {
+  cloud_type = 1
+  account_name = "devops"
+  gw_name = "avtxgw1"
+  vpc_id = "vpc-abcdef"
+  vpc_reg = "us-west-1"
+  vpc_size = "t2.micro"
+  vpc_net = "10.0.0.0/24"
+  tag_list = ["k1:v1","k2:v2"]
+}
+
+# Create Aviatrix AWS gateway to act as our on-prem / "Remote" server
+resource "aviatrix_gateway" "test_gateway1" {
+  cloud_type = 1
+  account_name = "devops"
+  gw_name = "avtxgw2"
+  vpc_id = "vpc-ghijkl"
+  vpc_reg = "us-east-1"
+  vpc_size = "t2.micro"
+  vpc_net = "11.0.0.0/24"
+  tag_list = ["k1:v1","k2:v2"]
+}
+
 resource "aviatrix_site2cloud" "s2c_test" {
   vpc_id = "${var.aws_vpc_id}"
   connection_name = "${var.avx_s2c_conn_name}"
@@ -15,4 +39,6 @@ resource "aviatrix_site2cloud" "s2c_test" {
   backup_pre_shared_key = "${var.pre_shared_key_backup}" # (Optional)
   remote_subnet_cidr = "${var.remote_subnet_cidr}" # on-prem's subnet cidr
   local_subnet_cidr = "${var.local_subnet_cidr}" # (Optional)
+
+  depends_on = ["aviatrix_gateway.test_gateway1", "aviatrix_gateway.test_gateway2"]
 }
