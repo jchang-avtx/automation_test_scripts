6bf1373d324892734dfc19e67af6444c9aaa8bee
Anthony <ant.lee813@gmail.com>
2019-03-11 16:57:04 -0700

    Updated vgw_conn's gw dependency

--- Terraform_Scripts/transit_network/vgw_conn/vgw_conn.tf
+++ Terraform_Scripts/transit_network/vgw_conn/vgw_conn.tf
@@ -4,10 +4,27 @@
 # from transit GW by destroying this resource using command:
 # terraform destroy --target aviatrix_vgw_conn.test_vgw_conn.
 
+# Need to create transit_vpc (gw) before establishing a vgw conn
+resource "aviatrix_transit_vpc" "test_transit_gw" {
+  cloud_type = 1
+  account_name = "devops"
+  gw_name = "testtransitGW1"
+  vpc_id = "vpc-abcd123"
+  vpc_reg = "us-east-1"
+  vpc_size = "t2.micro"
+  subnet = "10.1.0.0/24"
+  # ha_subnet = "10.1.0.0/24"
+  # ha_gw_size = "t2.micro"
+  tag_list = ["k1:v1", "k2:v2"]
+  enable_hybrid_connection = true
+  connected_transit = "yes"
+}
+
 resource "aviatrix_vgw_conn" "test_vgw_conn" {
   conn_name = "${var.tgw_vgw_conn_name}" # Connection name
   gw_name = "${var.aviatrix_gateway_name}" # existing transitGW name
   vpc_id = "${var.aws_vpc_id}" # VPC ID of transitGW
   bgp_vgw_id = "${var.aws_vgw_id}" # VGW ID
   bgp_local_as_num = "${var.bgp_local_as}"
+  depends_on = ["aviatrix_transit_vpc.test_transit_gw"]
 }
