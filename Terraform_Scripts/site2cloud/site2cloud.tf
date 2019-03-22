b75dc806f22f407a928df0471cf5e1ea4fd6487a
Anthony <ant.lee813@gmail.com>
2019-03-21 17:36:44 -0700

    Minor edit; clarified site2cloud key req
    ** commented out the preshared key because both keys are optional and will be autogenerated if not specified.
    ** only need to uncomment when testing that specific argument
    ** added comments for clarity in regards to backup arguments

--- Terraform_Scripts/site2cloud/site2cloud.tf
+++ Terraform_Scripts/site2cloud/site2cloud.tf
@@ -11,6 +11,8 @@ resource "aviatrix_gateway" "test_gateway1" {
   vpc_net = "10.20.1.0/24"
   allocate_new_eip = "off"
   eip = "6.6.6.6"
+  # peering_ha_subnet = xxxx # uncomment before creation to test s2c ha_enabled
+  # peering_ha_eip = xxxx # uncomment before creation to test s2c ha_enabled
 }
 
 # Create Aviatrix AWS gateway to act as our on-prem / "Remote" server
@@ -24,6 +26,8 @@ resource "aviatrix_gateway" "test_gateway2" {
   vpc_net = "10.23.0.0/24"
   allocate_new_eip = "off"
   eip = "5.5.5.5"
+  # peering_ha_subnet = xxxx # uncomment before creation to test s2c ha_enabled = "yes"
+  # peering_ha_eip = xxxx  # uncomment before creation to test s2c ha_enabled = "yes"
 }
 
 resource "aviatrix_site2cloud" "s2c_test" {
@@ -35,13 +39,13 @@ resource "aviatrix_site2cloud" "s2c_test" {
   ha_enabled = "${var.ha_enabled}" # (Optional) "yes" or "no"
 
   primary_cloud_gateway_name = "${var.avx_gw_name}" # local gw name
-  # backup_gateway_name = "${var.avx_gw_name_backup}" # (Optional)
+  # backup_gateway_name = "${var.avx_gw_name_backup}" # uncomment before creation to test s2c ha_enabled = "yes"
   remote_gateway_ip = "${var.remote_gw_ip}"
-  # backup_remote_gateway_ip = "${var.remote_gw_ip_backup}" # (Optional)
-  pre_shared_key = "${var.pre_shared_key}" # (Optional) Auto-generated if not specified
-  # backup_pre_shared_key = "${var.pre_shared_key_backup}" # (Optional)
+  # backup_remote_gateway_ip = "${var.remote_gw_ip_backup}" # uncomment before creation to test s2c ha_enabled = "yes"
+  # pre_shared_key = "${var.pre_shared_key}" # (Optional) Auto-generated if not specified
+  # backup_pre_shared_key = "${var.pre_shared_key_backup}" # (Optional) # can uncomment before creation to test s2c ha_enabled = "yes"
   remote_subnet_cidr = "${var.remote_subnet_cidr}" # on-prem's subnet cidr
   local_subnet_cidr = "${var.local_subnet_cidr}" # (Optional)
 
-  depends_on = ["aviatrix_gateway.test_gateway1", "aviatrix_gateway.test_gateway2"] # must comment out if testing backup parameters
+  depends_on = ["aviatrix_gateway.test_gateway1", "aviatrix_gateway.test_gateway2"]
 }
--- Terraform_Scripts/site2cloud/terraform.tfvars
+++ Terraform_Scripts/site2cloud/terraform.tfvars
@@ -14,10 +14,10 @@ avx_s2c_tunnel_type = "udp"
 ha_enabled = "no" # (optional)
 
 avx_gw_name = "avxPrimaryGwName"
-# avx_gw_name_backup = "avxPrimaryGwName" # (optional)
+# avx_gw_name_backup = "avxPrimaryGwName" # uncomment before creation to test s2c ha_enabled = "yes"
 remote_gw_ip = "5.5.5.5"
-# remote_gw_ip_backup = "4.4.4.4"
-pre_shared_key = "key1234" # (optional) Auto-generated if not specified
+# remote_gw_ip_backup = "4.4.4.4" # uncomment before creation to test s2c ha_enabled = "yes"
+# pre_shared_key = "key1234" # (optional) Auto-generated if not specified
 # pre_shared_key_backup = "backupkey5678" # (optional)
 remote_subnet_cidr = "10.23.0.0/24" # on-prem's subnet cidr
 local_subnet_cidr = "10.20.1.0/24" # (optional)
