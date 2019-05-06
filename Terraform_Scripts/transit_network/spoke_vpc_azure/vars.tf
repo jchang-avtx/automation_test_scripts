ee85304422a2b34f568fd3f5311841d1b1cb6377
Anthony <ant.lee813@gmail.com>
2019-05-06 14:25:52 -0700

    Added disableNAT test case for Azure spoke_vpc

--- Terraform_Scripts/transit_network/spoke_vpc_azure/vars.tf
+++ Terraform_Scripts/transit_network/spoke_vpc_azure/vars.tf
@@ -3,6 +3,7 @@ variable "arm_gw_size" {}
 variable "arm_ha_gw_subnet" {}
 variable "arm_ha_gw_size" {}
 
+variable "toggle_snat" {}
 variable "toggle_single_az_ha" {}
 
 variable "attached_transit_gw" {}
