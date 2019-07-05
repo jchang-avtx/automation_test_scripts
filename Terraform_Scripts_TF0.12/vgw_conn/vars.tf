469ab004f735bf11da7bc19084855952e373bdf6
Anthony <ant.lee813@gmail.com>
2019-07-05 15:24:09 -0700

    TF 0.12: Added vgw_conn, tunnel
    ++ vgw_conn
    ++ tunnel (peering tunnel)

---
+++ Terraform_Scripts_TF0.12/vgw_conn/vars.tf
@@ -0,0 +1,6 @@
+variable "aviatrix_controller_ip" {}
+variable "aviatrix_controller_username" {}
+variable "aviatrix_controller_password" {}
+
+variable "toggle_advertise_transit_cidr" {}
+variable "bgp_manual_spoke_advertise_cidrs_list" {}
