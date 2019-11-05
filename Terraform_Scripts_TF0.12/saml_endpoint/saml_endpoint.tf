73e4051b67e4cdfe3d6fdbc3805f53b24348663e
Anthony <ant.lee813@gmail.com>
2019-11-05 14:26:31 -0800

    TF 0.12: (11255) saml_endpoint custom_saml_request_template support added

--- Terraform_Scripts_TF0.12/saml_endpoint/saml_endpoint.tf
+++ Terraform_Scripts_TF0.12/saml_endpoint/saml_endpoint.tf
@@ -6,3 +6,11 @@ resource "aviatrix_saml_endpoint" "text_saml_endpoint" {
   idp_metadata          = "sample saml text heree"
   # custom_entity_id      = "customID" # blank if Hostname. Otherwise, set custom ID here
 }
+
+resource "aviatrix_saml_endpoint" "custom_saml_endpoint" {
+  endpoint_name         = "custom_saml_endpoint"
+  idp_metadata_type     = "Text"
+  idp_metadata          = "sample saml text part 2"
+  custom_entity_id      = "customID"
+  custom_saml_request_template = "${base64decode(var.saml_template)}"
+}
