28dc72f66f86516fbf6fd9f1bf99ca26dcf96a98
Anthony <ant.lee813@gmail.com>
2019-11-21 17:38:31 -0800

    TF 0.12: (9315) AWSGov account test case added

---
+++ Terraform_Scripts_TF0.12/account_awsgov/account_awsgov.tf
@@ -0,0 +1,7 @@
+resource "aviatrix_account" "gov_root" {
+  account_name            = "AWSGovRoot"
+  cloud_type              = 256
+  awsgov_account_number   = var.awsgov_account_num
+  awsgov_access_key       = var.awsgov_access_key
+  awsgov_secret_key       = var.awsgov_secret_key
+}
---
+++ Terraform_Scripts_TF0.12/account_awsgov/vars.tf
@@ -0,0 +1,7 @@
+variable "aviatrix_controller_ip" {}
+variable "aviatrix_controller_username" {}
+variable "aviatrix_controller_password" {}
+
+variable "awsgov_account_num" {}
+variable "awsgov_access_key" {}
+variable "awsgov_secret_key" {}
