93a86ba9ec4ee0879bdcb26616c0d611853b381c
jchang-avtx <jchang@aviatrix.com>
2019-12-31 11:29:09 -0800

    Create vars.tf

---
+++ Terraform_Solution_Tests/fqdn/vars.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aviatrix_controller_ip" {}
+variable "aviatrix_controller_username" {}
+variable "aviatrix_controller_password" {}
+variable "aviatrix_aws_access_account" {}
+
+variable "aviatrix_fqdn_mode" {}
+variable "aviatrix_fqdn_status" {}
+variable "aviatrix_fqdn_tag" {}
+variable "aviatrix_fqdn_gateway" {}
+
+variable "aviatrix_fqdn_source_ip_list" {
+  type = list(string)
+}
+variable "aviatrix_fqdn_domain" {
+  type = list(string)
+}
+variable "aviatrix_fqdn_protocol" {
+  type = list(string)
+}
+variable "aviatrix_fqdn_port" {
+  type = list(string)
+}
+
+variable "public_key" {}
