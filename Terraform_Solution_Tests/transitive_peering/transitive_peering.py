4cf8b0808afbc0e209ca27169e7d01987dc808b7
jchang-avtx <jchang@aviatrix.com>
2020-01-03 16:36:22 -0800

    Create  transitive_peering.py

---
+++ Terraform_Solution_Tests/transitive_peering/transitive_peering.py
@@ -0,0 +1,68 @@
+#! /usr/bin/python3
+
+import sys
+import time
+import re
+import os
+import getopt
+import subprocess
+
+def main(argv):
+  result = 'PASS'
+  ping_list = ''
+  try:
+    opts, args = getopt.getopt(argv,'h',['ping_list='])
+  except getopt.GetoptError:
+    print('usage: PROG [-h] --ping_list <list_of_ip_addresses>')
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+      print('usage: PROG [-h] --ping_list <list_of_ip_addresses>')
+      sys.exit()
+    elif opt in ('',"--ping_list"):
+        ping_list = arg.split(",")
+
+  i = 0
+  num_of_tries = 0
+  f = open('/tmp/log.txt','w')
+
+  '''
+    Try ping test for each IP.
+    If ping fails, it will re-try up to 3 times.
+  '''
+  while i < len(ping_list):
+    cmd = 'ping -c 3 ' + ping_list[i]
+    f.write('Trying to execute cmd ...\n')
+    output = subprocess.getoutput(cmd)
+    f.write('Successfully executed cmd ...\n')
+    m = re.search('3 received',output)
+    if m:
+      i += 1
+      num_of_tries = 0
+      f.write('#################### Below Test Passes ####################\n')
+      f.write('Test Passes for cmd >>>>> '+cmd+'\n')
+      f.write('###########################################################\n')
+    elif num_of_tries < 3:
+      num_of_tries += 1
+      f.write('###################### Traffic Failed ######################\n')
+      f.write('Executed cmd >>>>> '+cmd+'\n')
+      f.write('Number of Tries: '+str(num_of_tries)+'\n')
+      f.write('Will try again. Sleeping for sometime....\n')
+      time.sleep(num_of_tries*30)
+    else:
+      result = 'FAIL'
+      i += 1
+      num_of_tries = 0
+      f.write('****************  Test Failed after 3 tries ****************\n')
+      f.write('Executed cmd >>>>> '+cmd+'\n')
+      f.write('Output : \n')
+      f.write(output)
+      f.write('\n')
+      f.write('************************************************************\n')
+  f.close()
+
+  with open('/tmp/result.txt','w') as result_file:
+    result_file.write(result+'\n')
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
