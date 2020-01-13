7c9579d03f0a43b6a3f2c2d05b56db9b5943d680
Anthony <ant.lee813@gmail.com>
2020-01-13 11:34:09 -0800

    TGW DXC test case to use dynamic value
    ** cleaned up other tgw test cases to use env var

--- Terraform_Scripts_TF0.12/aws_tgw_directconnect/aws_tgw_directconnect.tf
+++ Terraform_Scripts_TF0.12/aws_tgw_directconnect/aws_tgw_directconnect.tf
@@ -1,9 +1,13 @@
 # depends on aviatrix_aws_tgw_vpc_attachment's tgw
+resource "aws_dx_gateway" "aws_tgw_dx_gw" {
+  name              = "aws-tgw-dx-gw"
+  amazon_side_asn   = 64512
+}
 
 resource "aviatrix_aws_tgw_directconnect" "aws_tgw_dc" {
   tgw_name                    = "testAWSTGW2"
   directconnect_account_name  = "AWSAccess"
-  dx_gateway_id               = "629dddaa-cc3d-41da-b54e-83bb43b6934b"
+  dx_gateway_id               = aws_dx_gateway.aws_tgw_dx_gw.id
   security_domain_name        = "SDN1"
   allowed_prefix              = var.prefix
 }
