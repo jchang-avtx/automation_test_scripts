b58379dbd8fb547f21c5fc1e8ffb81eba4d7a5b5
Anthony <ant.lee813@gmail.com>
2020-01-23 17:40:54 -0800

    Added trans_peer, transit_gateway (AWS, ARM, FireNet).py

---
+++ Terraform_Scripts_TF0.12/test_cases/transit_gateway/run_transit_gateway.py
@@ -0,0 +1,122 @@
+"""
+run_transit_gateway.py
+
+Test case for AWS Transit Gateway (Insane HA) Terraform resource/ use-case
+
+- note various placeholders that must be updated:
+    - filepath for terraform_fx.py
+    - create/ import/ update/ destroy test functions and respective arguments
+    - update "#" in "avx_ip_#", "avx_user_#", "avx_pass_#" accordingly
+- place xxx.py in respective test_cases/xxx/ directory
+"""
+
+import os
+
+import sys
+sys.path.insert(1, '/file_path/aviatrix_terraform_regression/py_libs')
+import terraform_fx as tf
+
+import logging
+import logging.handlers
+import time
+
+TIMESTR = time.strftime("%Y-%m-%d")
+LOGLEVEL = logging.DEBUG
+FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+DATEFORMAT = "%d-%b-%y %H:%M:%S"
+logging.basicConfig(level=LOGLEVEL,
+                    format=FORMAT,
+                    datefmt=DATEFORMAT,
+                    handlers=[
+                        logging.FileHandler(filename=TIMESTR + "_regression_result.log"),
+                        logging.StreamHandler()
+                    ])
+log = logging.getLogger()
+
+log.info("\n")
+log.info("============================================================")
+log.debug("RUNNING STAGE: " + str(os.path.split(os.getcwd())[1]).upper())
+log.info("============================================================")
+log.info("Steps to perform:")
+log.info("      1. Set up environment variables/ credentials")
+log.info("      2. Create AWS transit gateway (Insane HA)")
+log.info("      3. Perform terraform import to identify deltas")
+log.info("      4. Perform update tests on various attributes")
+log.info("      5. Tear down infrastructure\n")
+
+try:
+    log.info("Setting environment...")
+    log.debug("     placeholder_ip: %s", str(os.environ["AVIATRIX_CONTROLLER_IP"]))
+    log.debug("     placeholder_user: %s", str(os.environ["AVIATRIX_USERNAME"]))
+    log.debug("     placeholder_pass: %s", str(os.environ["AVIATRIX_PASSWORD"]))
+    avx_controller_ip = os.environ["avx_ip_1"]
+    avx_controller_user = os.environ["avx_user_1"]
+    avx_controller_pass = os.environ["avx_pass_1"]
+    log.info("Setting new variable values as follows...")
+    log.debug("     avx_controller_ip: %s", avx_controller_ip)
+    log.debug("     avx_controller_user: %s", avx_controller_user)
+    log.debug("     avx_controller_pass: %s", avx_controller_pass)
+    os.environ["AVIATRIX_CONTROLLER_IP"] = avx_controller_ip
+    os.environ["AVIATRIX_USERNAME"] = avx_controller_user
+    os.environ["AVIATRIX_PASSWORD"] = avx_controller_pass
+except:
+    log.info("-------------------- RESULT --------------------")
+    log.error("     Failed to properly set environment credentials!")
+    sys.exit()
+log.info("-------------------- RESULT --------------------")
+log.info("      Set environment credentials: PASS\n")
+
+
+try:
+    log.info("Creating infrastructure...")
+    tf.create_verify()
+except:
+    log.info("-------------------- RESULT --------------------")
+    log.error("     create_verify(): FAIL\n")
+    sys.exit()
+log.info("-------------------- RESULT --------------------")
+log.info("      create_verify(): PASS\n")
+
+
+try:
+    log.info("Verifying import functionality...")
+    tf.import_test("transit_gateway", "insane_transit_gw")
+except:
+    log.info("-------------------- RESULT --------------------")
+    log.error("     import_test(): FAIL\n")
+    sys.exit()
+log.info("-------------------- RESULT --------------------")
+log.info("      import_test(): PASS\n")
+
+
+try:
+    log.info("Verifying update functionality...")
+    log.debug("     enableSingleAZHA: Enabling single AZHA feature...")
+    tf.update_test("enableSingleAZHA")
+    log.debug("     switchConnectedTransit: Disabling spokes from running traffic to other spokes via transit gateway...")
+    tf.update_test("switchConnectedTransit")
+    log.debug("     disableHybrid: Disabling transit gateway from being used in AWS TGW solution...")
+    tf.update_test("disableHybrid")
+    log.debug("     updateGWSize: Updating gateway's size...")
+    tf.update_test("updateGWSize")
+    log.debug("     updateHAGWSize: Updating HA gateway's size...")
+    tf.update_test("updateHAGWSize")
+    log.debug("     enableDNSServer: Enabling feature to remove the default DNS server, in favor of VPC DNS server configured in VPC DHCP option...")
+    tf.update_test("enableDNSServer")
+except:
+    log.info("-------------------- RESULT --------------------")
+    log.error("     update_test(): FAIL\n")
+    sys.exit()
+log.info("-------------------- RESULT --------------------")
+log.info("      update_test(): PASS\n")
+
+
+try:
+    log.info("Verifying destroy functionality...")
+    tf.destroy_test()
+except:
+    log.info("-------------------- RESULT --------------------")
+    log.error("     destroy_test(): FAIL\n")
+    sys.exit()
+log.info("-------------------- RESULT --------------------")
+log.info("      destroy_test(): PASS\n")
