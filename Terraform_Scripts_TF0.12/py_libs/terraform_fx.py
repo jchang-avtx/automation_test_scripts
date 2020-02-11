f00da7be6cc94004c810d05e7377e104f3387e80
Anthony <ant.lee813@gmail.com>
2020-02-11 14:25:57 -0800

    Improve exception logging under py_libs
    ** add subprocess error obj reading

--- Terraform_Scripts_TF0.12/py_libs/terraform_fx.py
+++ Terraform_Scripts_TF0.12/py_libs/terraform_fx.py
@@ -8,19 +8,19 @@ def create_verify(varfile=None):
     try:
         if varfile == None:
             subprocess.run('terraform init', shell=True)
-            subprocess.run('terraform apply -auto-approve', shell=True, check=True)
-            subprocess.run('terraform plan', shell=True, check=True)
+            subprocess.run('terraform apply -auto-approve', shell=True, check=True, capture_output=True)
+            subprocess.run('terraform plan', shell=True, check=True, capture_output=True)
             subprocess.run('terraform show', shell=True)
         elif varfile:
             var_arg = '-var-file=' + varfile + '.tfvars'
             subprocess.run('terraform init', shell=True)
-            subprocess.run(['terraform', 'apply', var_arg, '-auto-approve'], check=True)
-            subprocess.run(['terraform', 'plan', var_arg], check=True)
+            subprocess.run(['terraform', 'apply', var_arg, '-auto-approve'], check=True, capture_output=True)
+            subprocess.run(['terraform', 'plan', var_arg], check=True, capture_output=True)
             subprocess.run('terraform show', shell=True)
         else:
             raise Exception('Too many arguments: create_verify(). # of args: {}'.format(len(args)))
-    except Exception as err:
-        log.exception(str(err))
+    except subprocess.CalledProcessError as err:
+        log.exception(str(err.stderr))
         sys.exit(1)
 
 
@@ -29,22 +29,22 @@ def import_test(resource, name, varfile=None):
         if varfile == None:
             resource_name = 'aviatrix_' + resource + '.' + name
             output_id = '"$(terraform output ' + name + '_id)"'
-            subprocess.run(['terraform', 'state', 'rm', resource_name], check=True)
-            subprocess.run('terraform import ' + resource_name + ' ' + output_id, shell=True, check=True)
-            subprocess.run('terraform plan', shell=True, check=True)
+            subprocess.run(['terraform', 'state', 'rm', resource_name], check=True, capture_output=True)
+            subprocess.run('terraform import ' + resource_name + ' ' + output_id, shell=True, check=True, capture_output=True)
+            subprocess.run('terraform plan', shell=True, check=True, capture_output=True)
             subprocess.run('terraform show', shell=True)
         elif varfile:
             resource_name = 'aviatrix_' + resource + '.' + name
             output_id = '"$(terraform output ' + name + '_id)"'
             var_arg = '-var-file=' + varfile + '.tfvars'
-            subprocess.run(['terraform', 'state', 'rm', resource_name], check=True)
-            subprocess.run('terraform import ' + var_arg + ' ' + resource_name + ' ' + output_id, shell=True, check=True)
-            subprocess.run(['terraform', 'plan', var_arg], check=True)
+            subprocess.run(['terraform', 'state', 'rm', resource_name], check=True, capture_output=True)
+            subprocess.run('terraform import ' + var_arg + ' ' + resource_name + ' ' + output_id, shell=True, check=True, capture_output=True)
+            subprocess.run(['terraform', 'plan', var_arg], check=True, capture_output=True)
             subprocess.run('terraform show', shell=True)
         else:
             raise Exception('Too many arguments: import_test()')
-    except Exception as err:
-        log.exception(str(err))
+    except subprocess.CalledProcessError as err:
+        log.exception(str(err.stderr))
         sys.exit(1)
 
 
@@ -52,19 +52,19 @@ def update_test(varfile, varfile2=None):
     try:
         if varfile2 == None:
             var_arg = '-var-file=' + varfile + '.tfvars'
-            subprocess.run(['terraform', 'apply', var_arg, '-auto-approve'], check=True)
-            subprocess.run(['terraform', 'plan', var_arg], check=True)
+            subprocess.run(['terraform', 'apply', var_arg, '-auto-approve'], check=True, capture_output=True)
+            subprocess.run(['terraform', 'plan', var_arg], check=True, capture_output=True)
             subprocess.run('terraform show', shell=True)
         elif varfile2:
             var_arg = '-var-file=' + varfile + '.tfvars'
             var_arg2 = '-var-file=' + varfile2 + '.tfvars'
-            subprocess.run(['terraform', 'apply', var_arg, var_arg2, '-auto-approve'], check=True)
-            subprocess.run(['terraform', 'plan', var_arg, var_arg2], check=True)
+            subprocess.run(['terraform', 'apply', var_arg, var_arg2, '-auto-approve'], check=True, capture_output=True)
+            subprocess.run(['terraform', 'plan', var_arg, var_arg2], check=True, capture_output=True)
             subprocess.run('terraform show', shell=True)
         else:
             raise Exception('Too many arguments: update_test()')
-    except Exception as err:
-        log.exception(str(err))
+    except subprocess.CalledProcessError as err:
+        log.exception(str(err.stderr))
         sys.exit(1)
 
 
@@ -75,20 +75,20 @@ def destroy_test(varfile=None):
             subprocess.run('terraform show', shell=True)
         elif varfile:
             var_arg = '-var-file=' + varfile + '.tfvars'
-            subprocess.run(['terraform', 'destroy', var_arg, '-auto-approve'], check=True)
+            subprocess.run(['terraform', 'destroy', var_arg, '-auto-approve'], check=True, capture_output=True)
             subprocess.run('terraform show', shell=True)
         else:
             raise Exception('Too many arguments: destroy_test()')
-    except Exception as err:
-        log.exception(str(err))
+    except subprocess.CalledProcessError as err:
+        log.exception(str(err.stderr))
         sys.exit(1)
 
 
 def destroy_target(resource, name):
     try:
         target_arg = '-target=aviatrix_' + resource + '.' + name
-        subprocess.run(['terraform', 'destroy', target_arg, '-auto-approve'], check=True)
+        subprocess.run(['terraform', 'destroy', target_arg, '-auto-approve'], check=True, capture_output=True)
         subprocess.run('terraform show', shell=True)
-    except Exception as err:
-        log.exception(str(err))
+    except subprocess.CalledProcessError as err:
+        log.exception(str(err.stderr))
         sys.exit(1)
