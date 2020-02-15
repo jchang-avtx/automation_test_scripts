fb0a9be8d475a5d741a8a9db93b2269332d4db13
Anthony <ant.lee813@gmail.com>
2020-02-14 18:27:31 -0800

    Try-except migration from py_lib for trans_peer and all transit_stages

--- Terraform_Scripts_TF0.12/test_cases/transit_gateway/run_transit_gateway.py
+++ Terraform_Scripts_TF0.12/test_cases/transit_gateway/run_transit_gateway.py
@@ -64,32 +64,35 @@ except Exception as err:
     log.info("-------------------- RESULT --------------------")
     log.error("     Failed to properly set environment credentials!")
     sys.exit(1)
-log.info("-------------------- RESULT --------------------")
-log.info("      Set environment credentials: PASS\n")
+else:
+    log.info("-------------------- RESULT --------------------")
+    log.info("      Set environment credentials: PASS\n")
 
 
 try:
     log.info("Creating infrastructure...")
     tf.create_verify()
-except Exception as err:
-    log.exception(str(err))
+except tf.subprocess.CalledProcessError as err:
+    log.exception(err.stderr.decode())
     log.info("-------------------- RESULT --------------------")
     log.error("     create_verify(): FAIL\n")
     sys.exit(1)
-log.info("-------------------- RESULT --------------------")
-log.info("      create_verify(): PASS\n")
+else:
+    log.info("-------------------- RESULT --------------------")
+    log.info("      create_verify(): PASS\n")
 
 
 try:
     log.info("Verifying import functionality...")
     tf.import_test("transit_gateway", "insane_transit_gw")
-except Exception as err:
-    log.exception(str(err))
+except tf.subprocess.CalledProcessError as err:
+    log.exception(err.stderr.decode())
     log.info("-------------------- RESULT --------------------")
     log.error("     import_test(): FAIL\n")
     sys.exit(1)
-log.info("-------------------- RESULT --------------------")
-log.info("      import_test(): PASS\n")
+else:
+    log.info("-------------------- RESULT --------------------")
+    log.info("      import_test(): PASS\n")
 
 
 try:
@@ -112,22 +115,24 @@ try:
     tf.update_test("updateFilterRoutes")
     log.debug("     updateExcludeAdvertiseRoutes: Updating list of VPC CIDRs to exclude from being advertised to on-prem...")
     tf.update_test("updateExcludeAdvertiseRoutes")
-except Exception as err:
-    log.exception(str(err))
+except tf.subprocess.CalledProcessError as err:
+    log.exception(err.stderr.decode())
     log.info("-------------------- RESULT --------------------")
     log.error("     update_test(): FAIL\n")
     sys.exit(1)
-log.info("-------------------- RESULT --------------------")
-log.info("      update_test(): PASS\n")
+else:
+    log.info("-------------------- RESULT --------------------")
+    log.info("      update_test(): PASS\n")
 
 
 try:
     log.info("Verifying destroy functionality...")
     tf.destroy_test()
-except Exception as err:
-    log.exception(str(err))
+except tf.subprocess.CalledProcessError as err:
+    log.exception(err.stderr.decode())
     log.info("-------------------- RESULT --------------------")
     log.error("     destroy_test(): FAIL\n")
     sys.exit(1)
-log.info("-------------------- RESULT --------------------")
-log.info("      destroy_test(): PASS\n")
+else:
+    log.info("-------------------- RESULT --------------------")
+    log.info("      destroy_test(): PASS\n")
