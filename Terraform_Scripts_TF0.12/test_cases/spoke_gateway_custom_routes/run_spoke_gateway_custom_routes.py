a897077921af6743c6c3f7f61ccb2e7f99226bf4
Anthony <ant.lee813@gmail.com>
2020-02-14 18:02:08 -0800

    Try-except migration from py_lib for all spoke_gateway stages

--- Terraform_Scripts_TF0.12/test_cases/spoke_gateway_custom_routes/run_spoke_gateway_custom_routes.py
+++ Terraform_Scripts_TF0.12/test_cases/spoke_gateway_custom_routes/run_spoke_gateway_custom_routes.py
@@ -64,20 +64,22 @@ except Exception as err:
     log.info("-------------------- RESULT --------------------")
     log.error("     Failed to properly set environment credentials!")
     sys.exit(1)
-log.info("-------------------- RESULT --------------------")
-log.info("      Set environment credentials: PASS\n")
+else:
+    log.info("-------------------- RESULT --------------------")
+    log.info("      Set environment credentials: PASS\n")
 
 
 try:
     log.info("Creating infrastructure...")
     tf.create_verify()
-except Exception as err:
-    log.exception(str(err))
+except tf.subprocess.CalledProcessError as err:
+    log.exception(err.stderr.decode())
     log.info("-------------------- RESULT --------------------")
     log.error("     create_verify(): FAIL\n")
     sys.exit(1)
-log.info("-------------------- RESULT --------------------")
-log.info("      create_verify(): PASS\n")
+else:
+    log.info("-------------------- RESULT --------------------")
+    log.info("      create_verify(): PASS\n")
 
 
 try:
@@ -90,13 +92,14 @@ try:
     tf.import_test("spoke_gateway", "gcp_custom_routes_spoke")
     log.debug("     Importing OCI spoke...")
     tf.import_test("spoke_gateway", "oci_custom_routes_spoke")
-except Exception as err:
-    log.exception(str(err))
+except tf.subprocess.CalledProcessError as err:
+    log.exception(err.stderr.decode())
     log.info("-------------------- RESULT --------------------")
     log.error("     import_test(): FAIL\n")
     sys.exit(1)
-log.info("-------------------- RESULT --------------------")
-log.info("      import_test(): PASS\n")
+else:
+    log.info("-------------------- RESULT --------------------")
+    log.info("      import_test(): PASS\n")
 
 
 try:
@@ -107,13 +110,14 @@ try:
     tf.update_test("updateFilterRoutes")
     log.debug("     updateIncludeAdvertiseRoutes: Updating list of VPC CIDRs to include from being advertised to on-prem...")
     tf.update_test("updateIncludeAdvertiseRoutes")
-except Exception as err:
-    log.exception(str(err))
+except tf.subprocess.CalledProcessError as err:
+    log.exception(err.stderr.decode())
     log.info("-------------------- RESULT --------------------")
     log.error("     update_test(): FAIL\n")
     sys.exit(1)
-log.info("-------------------- RESULT --------------------")
-log.info("      update_test(): PASS\n")
+else:
+    log.info("-------------------- RESULT --------------------")
+    log.info("      update_test(): PASS\n")
 
 
 try:
@@ -123,10 +127,11 @@ try:
     time.sleep(30)
     log.debug("     Continuing cleanup...")
     tf.destroy_test()
-except Exception as err:
-    log.exception(str(err))
+except tf.subprocess.CalledProcessError as err:
+    log.exception(err.stderr.decode())
     log.info("-------------------- RESULT --------------------")
     log.error("     destroy_test(): FAIL\n")
     sys.exit(1)
-log.info("-------------------- RESULT --------------------")
-log.info("      destroy_test(): PASS\n")
+else:
+    log.info("-------------------- RESULT --------------------")
+    log.info("      destroy_test(): PASS\n")
