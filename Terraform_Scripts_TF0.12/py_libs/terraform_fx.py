3d60601fde8cc27dbed6112916cd4b8063fab9d2
Anthony <ant.lee813@gmail.com>
2020-02-28 17:18:21 -0800

    Add capture_output and check for "terraform show" commands
    ++ capture_output and check=True for "terraform show"
    -- remove capture_output for "terraform plan" under import() in order to view possible delta outputs without failing the entire stage - (possible import deltas that are fine and ignored due to a refresh/apply that can rectify

--- Terraform_Scripts_TF0.12/py_libs/terraform_fx.py
+++ Terraform_Scripts_TF0.12/py_libs/terraform_fx.py
@@ -5,13 +5,13 @@ def create_verify(varfile=None):
         subprocess.run('terraform init', shell=True)
         subprocess.run('terraform apply -auto-approve', shell=True, check=True, capture_output=True)
         subprocess.run('terraform plan -detailed-exitcode', shell=True, check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     elif varfile:
         var_arg = '-var-file=' + varfile + '.tfvars'
         subprocess.run('terraform init', shell=True)
         subprocess.run(['terraform', 'apply', var_arg, '-auto-approve'], check=True, capture_output=True)
         subprocess.run(['terraform', 'plan', var_arg, '-detailed-exitcode'], check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     else:
         raise Exception('Too many arguments: create_verify(). # of args: {}'.format(len(args)))
 
@@ -22,14 +22,14 @@ def import_test(resource, name, varfile=None):
     if varfile == None:
         subprocess.run(['terraform', 'state', 'rm', resource_name], check=True, capture_output=True)
         subprocess.run('terraform import ' + resource_name + ' ' + output_id, shell=True, check=True, capture_output=True)
-        subprocess.run('terraform plan', shell=True, check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True)
+        subprocess.run('terraform plan', shell=True, check=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     elif varfile:
         var_arg = '-var-file=' + varfile + '.tfvars'
         subprocess.run(['terraform', 'state', 'rm', resource_name], check=True, capture_output=True)
         subprocess.run('terraform import ' + var_arg + ' ' + resource_name + ' ' + output_id, shell=True, check=True, capture_output=True)
-        subprocess.run(['terraform', 'plan', var_arg], check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True)
+        subprocess.run(['terraform', 'plan', var_arg], check=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     else:
         raise Exception('Too many arguments: import_test()')
 
@@ -39,12 +39,12 @@ def update_test(varfile, varfile2=None):
     if varfile2 == None:
         subprocess.run(['terraform', 'apply', var_arg, '-auto-approve'], check=True, capture_output=True)
         subprocess.run(['terraform', 'plan', var_arg, '-detailed-exitcode'], check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     elif varfile2:
         var_arg2 = '-var-file=' + varfile2 + '.tfvars'
         subprocess.run(['terraform', 'apply', var_arg, var_arg2, '-auto-approve'], check=True, capture_output=True)
         subprocess.run(['terraform', 'plan', var_arg, var_arg2, '-detailed-exitcode'], check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     else:
         raise Exception('Too many arguments: update_test()')
 
@@ -52,11 +52,11 @@ def update_test(varfile, varfile2=None):
 def destroy_test(varfile=None):
     if varfile == None:
         subprocess.run('terraform destroy -auto-approve', shell=True, check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     elif varfile:
         var_arg = '-var-file=' + varfile + '.tfvars'
         subprocess.run(['terraform', 'destroy', var_arg, '-auto-approve'], check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     else:
         raise Exception('Too many arguments: destroy_test()')
 
@@ -65,10 +65,10 @@ def destroy_target(resource, name, varfile=None):
     target_arg = '-target=aviatrix_' + resource + '.' + name
     if varfile == None:
         subprocess.run(['terraform', 'destroy', target_arg, '-auto-approve'], check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     elif varfile:
         var_arg = '-var-file=' + varfile + '.tfvars'
         subprocess.run(['terraform', 'destroy', target_arg, var_arg, '-auto-approve'], check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     else:
         raise Exception('Too many arguments: destroy_target()')
