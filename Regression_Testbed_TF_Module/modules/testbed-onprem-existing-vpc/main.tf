20650d2298735d35f926acc38040ce4620955ee8
Matthew <matthew@aviatrix.com>
2020-03-11 14:49:39 -0700

    change enable_snat to single_ip_snat for avtx gw

--- Regression_Testbed_TF_Module/modules/testbed-onprem-existing-vpc/main.tf
+++ Regression_Testbed_TF_Module/modules/testbed-onprem-existing-vpc/main.tf
@@ -9,7 +9,7 @@ resource "aviatrix_gateway" "avtx_gw" {
     vpc_reg             = data.aws_region.current.name
     gw_size             = "t3.micro"
     subnet              = var.pub_subnet_cidr
-    enable_snat         = true
+    single_ip_snat      = true
 }
 
 resource "aws_customer_gateway" "aws_cgw" {
