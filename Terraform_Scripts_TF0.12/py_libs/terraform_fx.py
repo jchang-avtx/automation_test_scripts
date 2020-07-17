b7f045a46822d57462795dd504b07438fe1f787d
Anthony <anthony@aviatrix.com>
2020-07-16 19:09:02 -0700

    Update terraform functions in library to support var argument
    ++ create and import function now support var argument
    ** new changes for CloudWAN stage to now support migration process of cred files off VCS in server

--- Terraform_Scripts_TF0.12/py_libs/terraform_fx.py
+++ Terraform_Scripts_TF0.12/py_libs/terraform_fx.py
@@ -1,51 +1,40 @@
 import subprocess
 
-def create_verify(varfile=None, timeout=None):
-    if varfile == None:
+def create_verify(varfile=None, timeout=None, varval=None):
+    if (varfile == None) and (varval == None):
         subprocess.run('terraform init', shell=True)
         subprocess.run('terraform apply -auto-approve', shell=True, check=True, capture_output=True, timeout=timeout)
         subprocess.run('terraform plan -detailed-exitcode', shell=True, check=True, capture_output=True)
         subprocess.run('terraform show', shell=True, check=True, capture_output=True)
-    elif varfile:
+    elif (varfile) and (varval == None):
         var_arg = '-var-file=' + varfile + '.tfvars'
         subprocess.run('terraform init', shell=True)
         subprocess.run(['terraform', 'apply', var_arg, '-auto-approve'], check=True, capture_output=True, timeout=timeout)
         subprocess.run(['terraform', 'plan', var_arg, '-detailed-exitcode'], check=True, capture_output=True)
         subprocess.run('terraform show', shell=True, check=True, capture_output=True)
+    elif (varfile) and (varval):
+        var_arg = '-var-file=' + varfile + '.tfvars'
+        subprocess.run('terraform init', shell=True)
+        subprocess.run(['terraform', 'apply', var_arg, '-var', varval, '-auto-approve'], check=True, capture_output=True, timeout=timeout)
+        subprocess.run(['terraform', 'plan', var_arg, '-var', varval, '-detailed-exitcode'], check=True, capture_output=True)
+        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     else:
         raise Exception('Too many arguments: create_verify(). # of args: {}'.format(len(args)))
 
 
-def create_var(varval, varval2=None):
-    var_arg = '-var ' + varval
-    if varval2 == None:
-        subprocess.run('terraform init', shell=True)
-        subprocess.run(['terraform', 'apply', var_arg, '-auto-approve'], check=True, capture_output=True)
-        subprocess.run(['terraform', 'plan', var_arg, '-detailed-exitcode'], check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
-    elif varval2:
-        var_arg2 = '-var ' + varval2
-        subprocess.run('terraform init', shell=True)
-        subprocess.run(['terraform', 'apply', var_arg, var_arg2, '-auto-approve'], check=True, capture_output=True)
-        subprocess.run(['terraform', 'plan', var_arg, var_arg2, '-detailed-exitcode'], check=True, capture_output=True)
-        subprocess.run('terraform show', shell=True, check=True, capture_output=True)
-    else:
-        raise Exception('Too many arguments: create_var(). # of args: {}'.format(len(args)))
-
-
-def import_test(resource, name, varfile=None):
+def import_test(resource, name, varfile=None, varval=None):
     resource_name = 'aviatrix_' + resource + '.' + name
     output_id = '"$(terraform output ' + name + '_id)"'
-    if varfile == None:
+    if (varfile == None) and (varval=None):
         subprocess.run(['terraform', 'state', 'rm', resource_name], check=True, capture_output=True)
         subprocess.run('terraform import ' + resource_name + ' ' + output_id, shell=True, check=True, capture_output=True)
         subprocess.run('terraform plan', shell=True, check=True)
         subprocess.run('terraform show', shell=True, check=True, capture_output=True)
-    elif varfile:
+    elif (varfile) and (varval):
         var_arg = '-var-file=' + varfile + '.tfvars'
         subprocess.run(['terraform', 'state', 'rm', resource_name], check=True, capture_output=True)
-        subprocess.run('terraform import ' + var_arg + ' ' + resource_name + ' ' + output_id, shell=True, check=True, capture_output=True)
-        subprocess.run(['terraform', 'plan', var_arg], check=True)
+        subprocess.run('terraform import ' + var_arg + ' ' + '-var ' + varval + ' ' + resource_name + ' ' + output_id, shell=True, check=True, capture_output=True)
+        subprocess.run(['terraform', 'plan', var_arg, '-var', varval], check=True)
         subprocess.run('terraform show', shell=True, check=True, capture_output=True)
     else:
         raise Exception('Too many arguments: import_test()')
