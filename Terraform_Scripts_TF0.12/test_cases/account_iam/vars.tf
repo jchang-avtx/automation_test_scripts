0c3c99717a095e5b13326ead8839ebe4007da98f
Anthony <anthony@aviatrix.com>
2020-08-05 15:36:05 -0700

    Refactor IAM account + change cred filepath references

--- Terraform_Scripts_TF0.12/test_cases/account_iam/vars.tf
+++ Terraform_Scripts_TF0.12/test_cases/account_iam/vars.tf
@@ -1,3 +1,3 @@
-variable "aws_account_number" {}
-variable "aws_iam_role_app" {}
-variable "aws_iam_role_ec2" {}
+variable aws_account_number {}
+variable aws_iam_role_app {}
+variable aws_iam_role_ec2 {}
